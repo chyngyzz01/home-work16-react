c9636f2f5a57ac2daed89196b6727a9d9158e2ce
Chika <chyngyzalkarbekov@gmail.com>
2022-08-15 18:58:13 +0600

    save-hw16

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { Component } from 'react'
+import './App.css'
+
+const DUMMY_DATA = [
+	{ id: 'm1', title: 'Junior' },
+	{ id: 'm2', title: 'Middle' },
+	{ id: 'm3', title: 'Senior' },
+]
+
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			items: [],
+		}
+	}
+	getInputValue = ({ target: { value } }) => {
+		this.setState((prevState) => {
+			return {
+				...prevState,
+				items:
+					DUMMY_DATA.some(
+						(item) =>
+							item.title.toLowerCase() === value.toLowerCase(),
+					) &&
+					!prevState.items.some(
+						(item) =>
+							item.title.toLowerCase() === value.toLowerCase(),
+					)
+						? prevState.items.concat({
+								title: value.toLowerCase(),
+								id: Math.random().toString(),
+						  })
+						: prevState.items,
+			}
+		})
+	}
+
+	render() {
+		return (
+			<div className='conatiner'>
+				{this.state.items.map((item) => {
+					return (
+						<div key={item.id} className='uppdatevalue'>
+							<h1>{item.title}</h1>
+						</div>
+					)
+				})}
+
+				<input
+					className='input'
+					type='text'
+					onChange={this.getInputValue}
+				/>
+				<div>
+					{DUMMY_DATA.map((item) => {
+						return (
+							<div className='static-items'>
+								<h1 key={item.id}>{item.title}</h1>
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ src/InputValue.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+
+class InputValue extends Component {
+	
+	render() {
+		return (
+			<div>
+				<h4>{this.props.name}</h4>
+			</div>
+		)
+	}
+}
+
+export default InputValue
